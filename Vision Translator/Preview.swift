940aad0435ac2454fb68896c8d5a9ba761c72bad
jamesgalante <jgalante@udel.edu>
2018-04-11 23:48:44 -0400

    Initial Commit

---
+++ Vision Translator/Preview.swift
@@ -0,0 +1,31 @@
+//
+//  preview.swift
+//  vision Translator
+//
+//  Created by James Galante on 4/11/18.
+//  Copyright © 2018 James Galante. All rights reserved.
+//
+
+
+import UIKit
+import AVFoundation
+class PreviewView: UIView {
+var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+    guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+        fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+    }
+    return layer
+}
+var session: AVCaptureSession? {
+    get {
+        return videoPreviewLayer.session
+    }
+    set {
+        videoPreviewLayer.session = newValue
+    }
+}
+// MARK: UIView
+override class var layerClass: AnyClass {
+    return AVCaptureVideoPreviewLayer.self
+}
+}
